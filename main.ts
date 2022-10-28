f283da15657c5575f45033df7d88f0f53addb55d
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-28 09:32:13 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,43 @@
-let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-let range = strip.range(0, 4)
+function RED () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Red))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(3, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function GREEN () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+    range = strip.range(3, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function YELLOW () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(3, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.setBrightness(70)
 basic.forever(function () {
-	
+    RED()
+    basic.pause(1000)
+    GREEN()
+    basic.pause(1000)
+    YELLOW()
+    basic.pause(1000)
+    RED()
 })
